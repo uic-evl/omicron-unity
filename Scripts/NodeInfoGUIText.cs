5f5f90f3033fe292c5a0cbf9d1aa81e288972bf1
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2014-07-29 20:27:37 -0500

    NEW: CAVE2 Multi-camera test

---
+++ Scripts/NodeInfoGUIText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeInfoGUIText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		guiText.text = "Node: "+System.Environment.MachineName;
+		guiText.text += "\nMain Camera Count: "+GameObject.FindGameObjectsWithTag("MainCamera").Length;
+	}
+}
