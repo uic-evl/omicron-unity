c2560103e487b4199af9b097a4d082f57a1b21c9
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2015-02-27 21:02:54 -0600

    NEW: CAVE2 skybox controller script

---
+++ Scripts/CAVE2SkyboxController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CAVE2SkyboxController : MonoBehaviour {
+
+    public LayerMask skyboxCullingMask;
+    LayerMask lastCullingMask;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if( lastCullingMask != skyboxCullingMask )
+        {
+            Camera[] cameras = gameObject.GetComponentsInChildren<Camera>();
+            foreach( Camera c in cameras )
+            {
+                c.cullingMask = skyboxCullingMask;
+            }
+            lastCullingMask = skyboxCullingMask;
+        }
+	}
+}
